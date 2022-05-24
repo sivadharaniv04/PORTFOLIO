a8555a7745ff91efa5529cabd438bcb97251e9e2
SIVADHARANI V <sivadharanivs@gmail.com>
2022-05-24 22:04:35 +0530

    https://github.com/sivadharaniv04/PORTFOLIO.git

---
+++ portfolio_vsd/src/components/timeline.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                      <h2>Internship studio <span>June 2021-July 2021</span></h2>
+                        <p>I have completed 5 weeks of internship in website designing with the title of designing an e-commerce portal at internship studio.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                      <h2>Undergraduation at Bannari Amman Institute Of Technology <span>2020-present</span></h2>
+                        <p>I am pursuing my under-graduation studies with major in CSE(Computer Science and Engineering). I have taken courses like DSA, DBMS, OS, Machine learning over the years and have better understanding of these subjects. I currently work as a graphic designer at the Code Chef Club in BIT.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Higher Secondary Education <span>2018-2020</span></h2>
+                        <p>I have completed my higher secondary education with major subjects as Physics, Chemistry & Maths with 77 percentage. During my time at school, I have developed interest in programming and mathematical skills to actually solve the problems.</p>  
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
